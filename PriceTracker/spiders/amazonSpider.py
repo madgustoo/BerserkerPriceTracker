051e17f5159e627de85fd9f447cf542f4599b057
madgustoo <agha97a@gmail.com>
2017-01-10 12:57:05 -0500

    Fixes to the amazon spider

--- PriceTracker/spiders/amazonSpider.py
+++ PriceTracker/spiders/amazonSpider.py
@@ -9,14 +9,16 @@ class AmazonSpider(scrapy.Spider):
     allowed_domains = ["amazon.ca"]
 
     start_urls = [
+        # Shorten this link
         "https://www.amazon.ca/s/ref=nb_sb_ss_i_1_7?url=search-alias%3Dstripbooks&field-keywords=berserk&sprefix=berserk%2Caps%2C140&crid=2EQUOUQ7COBVX"
     ]
 
     def parse(self, response):
-        # Section is an one amazon search result
-        for section in response.xpath('//*[@id="s-results-list-atf"]//li'):
+        # Section is a <li></li> amazon search result
+        for section in response.xpath('//*[@id="s-results-list-atf"]//li')
             item = AmazonItem()
-            item['name'] = response.xpath('//h2/text()').extract();
+            # https://doc.scrapy.org/en/latest/topics/selectors.html
+            item['name'] = section.xpath('.//h2/text()').extract();
             # item['price'] = response.xpath('//*[@id="result_0"]/div/div/div/div[2]/div[3]/div[1]/div[2]/a/span[2]/text()').extract()
             # item['publication_date'] = response.xpath('//*[@id="result_0"]/div/div/div/div[2]/div[2]/div[1]/span[3]/text()').extract()
             # item['image'] = response.xpath('//*[@id="result_0"]/div/div/div/div[1]/div/div/a/img/@src').extract()
