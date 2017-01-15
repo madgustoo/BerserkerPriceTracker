373d1a50a1f6e1f087f51649b5744184ebc88985
madgustoo <agha97a@gmail.com>
2017-01-14 23:23:56 -0500

    Separated the Amazon Spider into two bots, one to fetch the common data and the other to fetch the prices and availability

--- PriceTrackerSpider/spiders/amazonSpider.py
+++ PriceTrackerSpider/spiders/amazonSpider.py
@@ -1,90 +1,76 @@
 import re
 import scrapy
-import datetime
-from PriceTrackerSpider.items import AmazonItem, RetailerItem
-from volumes.models import Product
+from django.core.exceptions import ObjectDoesNotExist
+from PriceTrackerSpider.items import RetailerItem
+from volumes.models import Product, Retailer
 
 
-# Scrape amazon's canadian website to read and register the latest prices of berserk mangas to the API
+# This Spider will crawl once a day
+# Scrape amazon's canadian website to read and register PRICES and AVAILABILITY of berserk mangas to the API
 class AmazonSpider(scrapy.Spider):
+    retailer_name = "amazon.ca"
+    retailer_id = 1
     limit = 0
     name = "amazon"
     allowed_domains = ["amazon.ca"]
 
     start_urls = [
-        # Shorten this link
         "https://www.amazon.ca/s/ref=nb_sb_ss_i_2_6?url=search-alias%3Dstripbooks&field-keywords=berserk+manga"
     ]
 
-    # Section is an amazon search result, which is a div within the HTML class s-tem-container
     def parse(self, response):
         for section in response.xpath('//div[@class="s-item-container"]'):
-            item = AmazonItem()
+            retailer_item = RetailerItem()
             title = section.xpath('.//h2/text()').extract_first()
-            # Substitute multiple whitespace with a single whitespace
             name = ' '.join(title.split())
-            # ID is the volume's number / Gets extracted from the title then converted to an int
             product_id = ''.join(x for x in name if x.isdigit())
+
+            # TODO: find a way to overwrite or to check if it retailer already exists and simply update
 
             # Scrapes if Format: Berserk Volume 16
             if name.startswith("Berserk Volume") and name[-1:].isdigit():
-                item['name'] = name
-                item['id'] = product_id
+                # retailer_item['id'] = self.retailer_id
+                retailer_item['retailer_name'] = self.retailer_name
 
-                date = section.xpath('.//span[3][contains(@class, "a-color-secondary")]/text()').extract_first()
-                if len(date) > 4:
-                    publication_date = datetime.datetime.strptime(date, '%b %d %Y').date()
-                    item['publication_date'] = publication_date
+                # Gets the product with its id (product_id) and adds or updates its amazon details
+                try:
+                    retailer_item['product'] = Product.objects.get(id=product_id)
+                except ObjectDoesNotExist:
+                    print("Volume #" + product_id + " does not exist")
+                    pass
                 else:
-                    item['publication_date'] = None
+                    # If it does not raise an DoesNotExist exception
+                    cost = section.xpath('.//span[contains(@class, "s-price")]/text()').extract_first()
+                    if cost:
+                        # Remove CAD$ from the price
+                        price = re.sub('[CDN$]', '', cost)
+                        retailer_item['price'] = float(price)
+                    else:
+                        retailer_item['price'] = None
 
-                cost = section.xpath('.//span[contains(@class, "s-price")]/text()').extract_first()
-                if cost:
-                    # Remove CAD$ from the price
-                    price = re.sub('[CDN$]', '', cost)
-                    item['price'] = float(price)
-                else:
-                    item['price'] = None
+                    store_link = section.xpath('.//a/@href').extract_first()
+                    if store_link:
+                        retailer_item['store_link'] = store_link
+                    else:
+                        retailer_item['store_link'] = 0
 
-                image = section.xpath('.//img/@src').extract_first()
-                if image:
-                    item['image'] = image
-                else:
-                    item['image'] = None
+                    availability = section.xpath('.//div[contains(@class, "a-span7")]//div[4]//span/text()').extract_first()
+                    # Also checks if the xpath didn't select a price value [$], because of amazon's dom format, this is hard to predict and so this workaround does great
+                    if availability and "$" not in availability:
+                        retailer_item['availability'] = availability
+                    else:
+                        retailer_item['availability'] = "Not sold by Amazon.ca"
 
-                store_link = section.xpath('.//a/@href').extract_first()
-                if store_link:
-                    item['store_link'] = store_link
-                else:
-                    item['store_link'] = None
-
-                availability = section.xpath('.//div[contains(@class, "a-span7")]//div[4]//span/text()').extract_first()
-                if availability:
-                    item['availability'] = availability
-                else:
-                    item['availability'] = "Not sold by Amazon.ca"
-                # Return for json
-                # yield item
                 # Save to database
-                item.save()
-
-                # Separate Bot for retailers / Constant Id?
-                retailer_item = RetailerItem()
-                retailer_item['product'] = Product.objects.get(id=product_id)
-                retailer_item['retailer_name'] = "amazon"
-                retailer_item['store_link'] = "adfsd"
-                retailer_item['availability'] = "Sold out Amazon.ca"
-                retailer_item['price'] = 12.1
                 retailer_item.save()
 
-            # Crawl the next pages [limit = 3]
+        # Crawl the next pages [limit = 4]
         next_page = response.xpath('//span[contains(@class, "pagnLink")]//a/@href').extract()
         if next_page and self.limit < 4:
-            # If first page
             if self.limit == 0:
                 next_page_url = next_page[0]
             else:
                 next_page_url = next_page[1]
             self.limit += 1
-            request = scrapy.Request(url="https://www.amazon.ca"+next_page_url)
+            request = scrapy.Request(url="https://www.amazon.ca" + next_page_url)
             yield request
---
+++ PriceTrackerSpider/spiders/dataCrawler.py
@@ -0,0 +1,59 @@
+import re
+import scrapy
+import datetime
+from PriceTrackerSpider.items import AmazonItem, RetailerItem
+
+
+# This Spider will run once a month, at the end of each to gather new volumes and save them to the database
+# Scrape amazon's canadian website to read and register not (the latest prices), but common data (title, image, etc.) of berserk mangas to the API
+class DataSpider(scrapy.Spider):
+    limit = 0
+    name = "datacrawler"
+    allowed_domains = ["amazon.ca"]
+
+    start_urls = [
+        "https://www.amazon.ca/s/ref=nb_sb_ss_i_2_6?url=search-alias%3Dstripbooks&field-keywords=berserk+manga"
+    ]
+
+    # Section is an amazon search result, which is a div within the HTML class s-tem-container
+    def parse(self, response):
+        for section in response.xpath('//div[@class="s-item-container"]'):
+            item = AmazonItem()
+            title = section.xpath('.//h2/text()').extract_first()
+            # Substitute multiple whitespace with a single whitespace
+            name = ' '.join(title.split())
+            # ID is the volume's number / Gets extracted from the title then converted to an int
+            product_id = ''.join(x for x in name if x.isdigit())
+
+            # Scrapes if Format: Berserk Volume 16
+            if name.startswith("Berserk Volume") and name[-1:].isdigit():
+                item['name'] = name
+                item['id'] = product_id
+
+                date = section.xpath('.//span[3][contains(@class, "a-color-secondary")]/text()').extract_first()
+                if len(date) > 4:
+                    publication_date = datetime.datetime.strptime(date, '%b %d %Y').date()
+                    item['publication_date'] = publication_date
+                else:
+                    item['publication_date'] = None
+
+                image = section.xpath('.//img/@src').extract_first()
+                if image:
+                    item['image'] = image
+                else:
+                    item['image'] = None
+
+                # Save to database
+                item.save()
+
+        # Crawl the next pages [limit = 4]
+        next_page = response.xpath('//span[contains(@class, "pagnLink")]//a/@href').extract()
+        if next_page and self.limit < 4:
+            # If first page
+            if self.limit == 0:
+                next_page_url = next_page[0]
+            else:
+                next_page_url = next_page[1]
+            self.limit += 1
+            request = scrapy.Request(url="https://www.amazon.ca" + next_page_url)
+            yield request
